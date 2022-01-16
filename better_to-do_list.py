93fb0fd3bfbe560d85cd0394df5160b28b9901da
B P <bp@bp.com>
2022-01-16 16:05:28 +0100

    autosave

--- better_to-do_list.py
+++ better_to-do_list.py
@@ -1,5 +1,5 @@
 # Modyfikuję to-do list, którą została przedstawiona w tym filmie: https://youtu.be/BBu6ZoAHIwI?t=6624
-
+import itertools
 import os.path
 
 tasks = {}
@@ -31,6 +31,46 @@ def delete_task():
         print("\nThis task doesn't exist\n")
 
 
+def autosave(save_global):
+    save = save_global
+
+    if save is False:
+        turn_off = input("Autosave is turn off. If you want to turn it on enter 'turn on' if not enter 'no': ")
+        print()
+        if turn_off == "turn on":
+            return True
+        elif turn_off == "no":
+            print("The settings have not been changed")
+            print()
+            return save
+        else:
+            print("Invalid value")
+            print()
+            return save
+
+    elif save is True:
+        turn_on = input("Autosave is turn on. If you want to turn it off enter 'turn off' if not enter 'no': ")
+        print()
+        if turn_on == "turn off":
+            return False
+        elif turn_on == "no":
+            print("The settings have not been changed")
+            print()
+            return save
+        else:
+            print("Invalid value")
+            print()
+            return save
+
+
+def save_tasks(name_f):
+    file = open(name_f, "w")
+    for task in tasks:
+        a = tasks[task]
+        file.write(task + "\n" + a + "\n")
+    file.close()
+
+
 def save_tasks_to_file():
     answer = input("If you want to save tasks in an existing file check 1 or create a new file (check 2): \n")
     if answer == "1":
@@ -38,24 +78,38 @@ def save_tasks_to_file():
         m = os.path.isfile("./" + file_name)
         if m is True:
             print("\nThe tasks have been successfully saved\n")
-            file = open(file_name, "w")
-            for task in tasks:
-                a = tasks[task]
-                file.write(task + "\n" + a + "\n")
-            file.close()
+            save_tasks(file_name)
         else:
             print("\nThe file was not found\n")
 
+        return file_name
+
     elif answer == "2":
         file_name = input("Enter the name of the new file: ")
-        file = open(file_name, "w")
-        for task in tasks:
-            a = tasks[task]
-            file.write(task + "\n" + a + "\n")
-
+        save_tasks(file_name)
         print("\nThe tasks have been successfully saved the new file" + str(file_name))
+
+        return file_name
+
+    else:
         print()
-        file.close()
+        print("Invalid value")
+        print()
+
+
+def save_after_exit(name, optional_s):
+    if name != "" and optional_s is True:
+        save_tasks(name)
+    elif name == "":
+        if os.path.isfile("./" + "new_save.txt") is False:
+            save_tasks("new_save.txt")
+        else:
+            for number in itertools.count(start=2):
+                if os.path.isfile("./" + "new_save_" + str(number) + ".txt") is False:
+                    save_tasks("new_save_" + str(number) + ".txt")
+                    break
+                else:
+                    continue
 
 
 def load_tasks_from_file():
@@ -90,11 +144,14 @@ def show_menu():
     print("3. Remove task")
     print("4. Save changes to file")
     print("5. Load changes from file")
-    print("6. Exit")
+    print("6. Autosave")
+    print("7. Exit")
 
 
 show_menu()
 user_choice = 1
+name_of_the_file = ""
+auto_s = False
 
 while True:
     try:
@@ -106,27 +163,32 @@ while True:
             show_tasks()
             show_menu()
 
-        if user_choice == 2:
+        elif user_choice == 2:
             add_task()
             show_menu()
 
-        if user_choice == 3:
+        elif user_choice == 3:
             delete_task()
             show_menu()
 
-        if user_choice == 4:
-            save_tasks_to_file()
+        elif user_choice == 4:
+            name_of_the_file = save_tasks_to_file()
             show_menu()
 
-        if user_choice == 5:
+        elif user_choice == 5:
             load_tasks_from_file()
             show_menu()
 
-        if user_choice == 6:
+        elif user_choice == 6:
+            auto_s = autosave(auto_s)
+            show_menu()
+
+        elif user_choice == 7:
+            save_after_exit(name_of_the_file, auto_s)
             print("Exit")
             break
 
-        elif user_choice >= 6 or user_choice <= 0:
+        elif user_choice <= 0 or user_choice >= 7:
             print("Invalid Value!")
             print()
             show_menu()
